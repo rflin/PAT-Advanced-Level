e0bdb48c104f003f42372c1c3ad5d28b87fd4686
L <462650965@qq.com>
2017-09-07 20:06:32 +0800

    -

---
+++ 1004. Counting Leaves (30).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> v[128],ans;
+void solve(int r,unsigned int lev)
+{
+	if(lev+1>ans.size()) ans.push_back(0);
+	if(v[r].size()==0) ans[lev]++;
+	for(auto x:v[r])
+		solve(x,lev+1);
+}
+int main(int argc, char const *argv[])
+{
+	int n,m;
+	cin>>n>>m;
+	for(int i=0;i<m;++i)
+	{
+		int id,k,idx;
+		cin>>id>>k;
+		for(int j=0;j<k;++j)
+		{
+			cin>>idx;
+			v[id].push_back(idx);
+		}
+	}
+	solve(1,0);
+	for(unsigned int i=0;i<ans.size();++i)
+	{
+		i!=ans.size()-1?cout<<ans[i]<<" ":cout<<ans[i];
+	}
+	return 0;
+}
