c14b76130523dded6213b7d710e1e82332792dd2
L <462650965@qq.com>
2017-09-10 14:23:29 +0800

    -

---
+++ 1020. Tree Traversals (25).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int n,s;
+vector<int> ino,pos;
+vector<vector<int>> lev; 
+void build(int posL,int posR,int inoL,int inoR,int dep)
+{
+	if(posL>posR) return;
+	int e=pos[posR],idx=inoR,pdx=posR;
+	while(idx>=inoL&&e!=ino[idx]) idx--,pdx--;
+	if(dep+1>lev.size()) lev.resize(dep+1);
+	lev[dep].push_back(e);
+	build(posL,pdx-1,inoL,idx-1,dep+1);
+	build(pdx,posR-1,idx+1,inoR,dep+1);
+}
+int main()
+{
+	cin>>n;
+	ino.resize(n);
+	pos.resize(n);
+	for(int i=0;i<n;++i) cin>>pos[i];
+	for(int i=0;i<n;++i) cin>>ino[i];
+	build(0,n-1,0,n-1,0);
+	for(auto xv:lev)
+	{
+		for(auto x:xv) ++s==n?cout<<x:cout<<x<<" ";
+	}
+	return 0;
+}
