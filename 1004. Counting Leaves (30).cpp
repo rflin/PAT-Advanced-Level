13a2f20af355ab153081f21ed32273c73b413375
L <462650965@qq.com>
2017-09-12 19:58:57 +0800

    -

--- 1004. Counting Leaves (30).cpp
+++ 1004. Counting Leaves (30).cpp
@@ -2,12 +2,12 @@
 #include <vector>
 using namespace std;
 vector<int> v[128],ans;
-void solve(int r,unsigned int lev)
+void countLeaves(int r,unsigned int lev)
 {
 	if(lev+1>ans.size()) ans.push_back(0);
 	if(v[r].size()==0) ans[lev]++;
 	for(auto x:v[r])
-		solve(x,lev+1);
+		countLeaves(x,lev+1);
 }
 int main(int argc, char const *argv[])
 {
@@ -23,7 +23,7 @@ int main(int argc, char const *argv[])
 			v[id].push_back(idx);
 		}
 	}
-	solve(1,0);
+	countLeaves(1,0);
 	for(unsigned int i=0;i<ans.size();++i)
 	{
 		i!=ans.size()-1?cout<<ans[i]<<" ":cout<<ans[i];
