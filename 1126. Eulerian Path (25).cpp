63912608fc27dc0cd877b1795a733d9bf13462a2
L <462650965@qq.com>
2017-09-12 09:41:22 +0800

    -

--- 1126. Eulerian Path (25).cpp
+++ 1126. Eulerian Path (25).cpp
@@ -24,16 +24,16 @@ int main()
     	cin>>u>>w;
     	v[u].push_back(w);
     	v[w].push_back(u);
-	}
-	dfs(1);
-	int odd=0;
-	for(int i=1;i<=n;++i)
-	{
-		i==n?cout<<v[i].size()<<endl:cout<<v[i].size()<<" ";
-		if(v[i].size()%2==1) odd++;
-	} 
-	if(odd==0&&cnt==n) cout<<"Eulerian";
-	else if(odd==2&&cnt==n) cout<<"Semi-Eulerian";
-	else cout<<"Non-Eulerian";
+    }
+    dfs(1);
+    int odd=0;
+    for(int i=1;i<=n;++i)
+    {
+    	i==n?cout<<v[i].size()<<endl:cout<<v[i].size()<<" ";
+    	if(v[i].size()%2==1) odd++;
+    }
+    if(odd==0&&cnt==n) cout<<"Eulerian";
+    else if(odd==2&&cnt==n) cout<<"Semi-Eulerian";
+    else cout<<"Non-Eulerian";
     return 0;
 }
