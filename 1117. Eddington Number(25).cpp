415593484d17fb798730454606c7ac733864d930
L <462650965@qq.com>
2017-09-13 21:05:15 +0800

    -

---
+++ 1117. Eddington Number(25).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int n,k=0,days=0;
+	cin>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;++i) cin>>v[i];
+	sort(v.begin(),v.end(),greater<int>());
+	while(k<n)
+	{
+		++days;
+		if(v[k]<=days) break;
+		++k;
+	}
+	k==n?cout<<n:cout<<days-1;
+	return 0;
+}
