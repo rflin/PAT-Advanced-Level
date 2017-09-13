2ab85b9b2952855b3525c1f6ddb3e9212ae33f0f
L <462650965@qq.com>
2017-09-13 18:36:10 +0800

    -

---
+++ 1129. Recommendation System (25).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set> 
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+struct node
+{
+	int idx,cnt;
+	node(int x,int y):idx(x),cnt(y){}
+	bool operator <(const node &x)const
+	{
+		if(cnt!=x.cnt) return cnt>x.cnt;
+		return idx<x.idx;
+	}
+};
+int main(int argc, char const *argv[])
+{
+	int n,k;
+	set<node> mset;
+	unordered_map<int,int> cnt;
+	cin>>n>>k;
+	for(int i=0;i<n;++i)
+	{
+		int idx;
+		cin>>idx;
+		if(i)
+		{
+			cout<<idx<<":";
+			int w=0;
+			for(auto it=mset.begin();it!=mset.end()&&++w<=k;++it) cout<<" "<<it->idx;
+			cout<<endl;
+		}
+		auto it=mset.find(node(idx,cnt[idx]));
+		if(it!=mset.end()) mset.erase(it);
+		mset.insert(node(idx,++cnt[idx]));
+	} 
+	return 0;
+}
