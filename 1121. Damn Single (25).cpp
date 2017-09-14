0cb266a02c069e39034d64d5460ae011fa098db9
L <462650965@qq.com>
2017-09-14 18:40:32 +0800

    -

--- 1121. Damn Single (25).cpp
+++ 1121. Damn Single (25).cpp
@@ -19,20 +19,20 @@ int main()
     	ishere[id2]=false;
     	couples[id1]=id2;
     	couples[id2]=id1;
-	}
-	cin>>m;
-	guests.resize(m);
-	for(int i=0;i<m;++i)
-	{
-		cin>>guests[i];
-		ishere[guests[i]]=true;
-	} 
-	for(auto id:guests)
-	{
-		if(!(couples.count(id)&&ishere[couples[id]])) dsv.push_back(id);
-	}
-	sort(dsv.begin(),dsv.end());
-	cout<<dsv.size()<<endl;
-	for(unsigned int i=0;i<dsv.size();++i) i==dsv.size()-1?printf("%05d",dsv[i]):printf("%05d ",dsv[i]);
+    }
+    cin>>m;
+    guests.resize(m);
+    for(int i=0;i<m;++i)
+    {
+    	cin>>guests[i];
+    	ishere[guests[i]]=true;
+    } 
+    for(auto id:guests)
+    {
+	if(!(couples.count(id)&&ishere[couples[id]])) dsv.push_back(id);
+    }
+    sort(dsv.begin(),dsv.end());
+    cout<<dsv.size()<<endl;
+    for(unsigned int i=0;i<dsv.size();++i) i==dsv.size()-1?printf("%05d",dsv[i]):printf("%05d ",dsv[i]);
     return 0;
 }
