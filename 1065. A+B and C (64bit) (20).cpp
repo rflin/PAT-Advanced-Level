19e57c390c1541a532de2a1e2cfd74412690babc
L <462650965@qq.com>
2017-12-26 18:20:44 +0800

    -

---
+++ 1065. A+B and C (64bit) (20).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;++i)
+    {
+        long long a,b,c;
+        cin>>a>>b>>c;
+        long long sum=a+b;
+        bool ret;
+        if(a<0&&b<0&&sum>=0) ret=0;
+        else if(a>0&&b>0&&sum<=0) ret=1;
+        else ret=(sum>c)?1:0;
+        ret?cout<<"Case #"<<i<<": true\n":cout<<"Case #"<<i<<": false\n";
+    }
+    return 0;
+}
