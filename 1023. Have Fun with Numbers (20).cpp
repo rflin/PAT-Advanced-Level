6b4303a2946678c4e680fbe5070186cd2376bea3
L <462650965@qq.com>
2017-12-28 16:45:38 +0800

    -

---
+++ 1023. Have Fun with Numbers (20).cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+string doubleNum(const string &n)
+{
+    int add=0;
+    string dn;
+    for(int i=n.size()-1;i>=0;--i)
+    {
+        int x=n[i]-'0';
+        int num=x*2+add;
+        add=num/10;
+        dn+=num%10+'0';
+    }
+    if(add>0)dn+=add+'0';
+    reverse(dn.begin(),dn.end());
+    return dn;
+}
+bool judge(string &a,string &b)
+{
+    vector<int> v(10,0);
+    for(auto x:a)
+        v[x-'0']++;
+    for(auto x:b)
+        v[x-'0']--;
+    for(auto x:v)
+        if(x)return false;
+    return true;
+}
+int main()
+{
+    string a,b;
+    vector<int> v(10,0);
+    cin>>a;
+    b=doubleNum(a);
+    if(judge(a,b))cout<<"Yes\n"<<b<<endl;
+    else cout<<"No\n"<<b<<endl;
+    return 0;
+}
