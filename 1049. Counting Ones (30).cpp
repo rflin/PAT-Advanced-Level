7f0dd104338366aee3721cd413d7e0005f0d6d7a
L <462650965@qq.com>
2017-12-29 18:54:37 +0800

    -

---
+++ 1049. Counting Ones (30).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+using namespace std;
+int main()
+{
+    string n;
+    cin>>n;
+    int ans=0;
+    for(int i=0;i!=(int)n.size();++i)
+    {
+        int mid=n[i]-'0';
+        int left=atoi(n.substr(0,i).c_str());
+        int right=atoi(n.substr(i+1).c_str());
+        int ex=n.size()-i-1;
+        if(mid==1)ans+=left*pow(10,ex)+right+1;
+        else if(mid==0)ans+=left*pow(10,ex);
+        else ans+=(left+1)*pow(10,ex);
+    }
+    cout<<ans;
+    return 0;
+}
