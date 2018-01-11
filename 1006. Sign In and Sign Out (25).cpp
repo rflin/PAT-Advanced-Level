1b9d1cdde535308a5f906c8dc89919c593441ad3
L <462650965@qq.com>
2018-01-11 20:19:53 +0800

    -

--- 1006. Sign In and Sign Out (25).cpp
+++ 1006. Sign In and Sign Out (25).cpp
@@ -1,21 +1,25 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int n,ft=24*3600+1,lt=-1;
-	char first[32],last[32];
-	scanf("%d\n",&n);
-	for(int i=0;i<n;++i)
+	int m;
+	string fname,lname,fst("24:00:00"),lst("00:00:00");
+	cin>>m;
+	while(m--)
 	{
-		char s[32];
-		int h1,m1,s1,h2,m2,s2;
-		scanf("%s %d:%d:%d %d:%d:%d",s,&h1,&m1,&s1,&h2,&m2,&s2);
-		int t1=h1*3600+m1*60+s1,t2=h2*3600+m2*60+s2;
-		if(t1<ft) ft=t1,strcpy(first,s);
-		if(t2>lt) lt=t2,strcpy(last,s);
+		string name,intime,outtime;
+		cin>>name>>intime>>outtime;
+		if(intime<=fst)
+		{
+			fst=intime;
+			fname=name;
+		}
+		if(outtime>=lst)
+		{
+			lst=outtime;
+			lname=name;
+		}
 	}
-	printf("%s %s",first,last);
+	cout<<fname<<" "<<lname<<endl;
 	return 0;
 }
