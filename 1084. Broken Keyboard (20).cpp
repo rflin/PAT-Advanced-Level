6e936ad5fe84728ba607c6e2f71a66f5df0af525
L <462650965@qq.com>
2018-02-23 14:16:15 +0800

    -

--- 1084. Broken Keyboard (20).cpp
+++ 1084. Broken Keyboard (20).cpp
@@ -1,35 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-void toCapital(string &str)
-{
-	for(auto &x:str)
-		if(islower(x))x=toupper(x);
-}
+int mark[128],isprt[128];
 int main()
 {
-	string a,b;
-	vector<bool> isOutput(128,0);
-	cin>>a>>b;
-	toCapital(a);
-	toCapital(b);
-	size_t i=0,j=0;
-	while(i<a.size()&&j<b.size())
+	string instr,outstr;
+	cin>>instr>>outstr;
+	for(char &x:outstr)
 	{
-		if(a[i]!=b[j])
-		{
-			if(isOutput[a[i]-'\0']==false)
-			{
-				isOutput[a[i]-'\0']=true;
-				cout<<a[i];
-			}
-			++i;
-		}
-		else ++i,++j;
+		if(isalpha(x)) x=toupper(x);
+		mark[x-'\0']=1;
 	}
-	while(i<a.size())
+	for(char &x:instr)
 	{
-		if(!isOutput[a[i]-'\0'])cout<<a[i];
-		++i;
+		if(isalpha(x)) x=toupper(x);
+		if(!mark[x-'\0']&&!isprt[x-'\0']) putchar(x),isprt[x-'\0']=1;
 	}
 	return 0;
 }
