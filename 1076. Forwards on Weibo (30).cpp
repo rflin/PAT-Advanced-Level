13902926eea690fe93a0df1226e351c09c7ca2b7
L <462650965@qq.com>
2018-02-24 13:50:28 +0800

    -

--- 1076. Forwards on Weibo (30).cpp
+++ 1076. Forwards on Weibo (30).cpp
@@ -1,54 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> v;
-int n,L,k,idx,q;
+vector<int> v[1024];
+int n,L,q,idx;
+int bfs(int r)
+{
+    queue<int> q;
+    q.push(r);
+    int len=1,level=0,result=0,visit[1024]={};
+    visit[r]=1;
+    while(len>0)
+    {
+        int nextLen=0;
+        for(int i=0;i<len;++i)
+        {
+            int x=q.front();
+            q.pop();
+            for(int w:v[x])
+                if(visit[w]==0) q.push(w),visit[w]=1,++nextLen,++result;
+        }
+        len=nextLen;
+        ++level;
+        if(level>=L) break;
+    }
+    return result;
+}
 int main()
 {
-	scanf("%d %d",&n,&L);
-	v.resize(n+1);
-	for(int i=1;i<=n;++i)
-	{
-		scanf("%d",&k);
-		for(int j=0;j<k;++j)
-		{
-			scanf("%d",&idx);
-			v[idx].push_back(i);
-		}
-	}
-	scanf("%d",&q);
-	while(q--)
-	{
-		scanf("%d",&idx);
-		bool visit[1024]={0};
-		int first=0,last=1,cnt=1,lev=0;
-		queue<int> q;
-		q.push(idx);
-		visit[idx]=1;
-		while(!q.empty())
-		{
-			int s=q.front();
-			q.pop();++first;
-			for(auto x:v[s])
-			{
-				if(!visit[x])
-				{
-					q.push(x);
-					visit[x]=1;
-					++cnt;
-				}
-			}
-			if(first==last)
-			{
-				++lev;
-				last=cnt;
-			}
-			if(lev==L) break;
-		}
-		cout<<cnt-1<<endl;
-	}
-	return 0;
+    scanf("%d %d",&n,&L);
+    for(int i=1;i<=n;++i)
+    {
+        int k;
+        scanf("%d",&k);
+        while(k--)
+        {
+            scanf("%d",&idx);
+            v[idx].push_back(i);
+        }
+    }
+    scanf("%d",&q);
+    while(q--)
+    {
+        scanf("%d",&idx);
+        printf("%d\n",bfs(idx));
+    }
+    return 0;
 }
