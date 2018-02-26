e0b9098c8d8d105b351d853de608c7e5f8c77fb3
L <462650965@qq.com>
2018-02-26 12:43:18 +0800

    -

--- 1081. Rational Sum (20).cpp
+++ 1081. Rational Sum (20).cpp
@@ -3,39 +3,39 @@ using namespace std;
 typedef long long llt;
 struct Rational
 {
-	llt i,a,b;
-	Rational(llt _i,llt _a,llt _b):i(_i),a(_a),b(_b){}
-	Rational & operator +=(const Rational &val)
-	{
-		llt x=a*val.b+b*val.a,y=b*val.b;
-		i+=val.i,a=x,b=y;
-		simplify();
-		return *this;
-	}
-	void simplify()
-	{
-		i+=a/b,a%=b;
-		llt v=abs(__gcd(a,b));
-		a/=v,b/=v;
-		if(a<0) i-=1,a+=b;
-	}
+    llt i,a,b;
+    Rational(llt _i,llt _a,llt _b):i(_i),a(_a),b(_b){}
+    Rational & operator +=(const Rational &val)
+    {
+        llt x=a*val.b+b*val.a,y=b*val.b;
+        i+=val.i,a=x,b=y;
+        simplify();
+        return *this;
+    }
+    void simplify()
+    {
+        i+=a/b,a%=b;
+        llt v=abs(__gcd(a,b));
+        a/=v,b/=v;
+        if(i<0&&a>0) i+=1,a-=b;
+    }
 };
 int main()
 {
-	int n;
-	Rational result(0,0,1);
-	scanf("%d",&n);
-	while(n--)
-	{
-		Rational val(0,0,1);
-		scanf("%lld/%lld",&val.a,&val.b);
-		val.simplify();
-		result+=val;
-	}
-	if(result.i&&result.a)
-		printf("%lld %lld/%lld\n",result.i,result.a,result.b);
-	else if(!result.i&&result.a)
-		printf("%lld/%lld\n",result.a,result.b);
-	else printf("%lld\n",result.i);
-	return 0;
+    int n;
+    Rational result(0,0,1);
+    scanf("%d",&n);
+    while(n--)
+    {
+        Rational val(0,0,1);
+        scanf("%lld/%lld",&val.a,&val.b);
+        val.simplify();
+        result+=val;
+    }
+    if(result.i&&result.a)
+        printf("%lld %lld/%lld\n",result.i,abs(result.a),result.b);
+    else if(!result.i&&result.a)
+        printf("%lld/%lld\n",result.a,result.b);
+    else printf("%lld\n",result.i);
+    return 0;
 }
