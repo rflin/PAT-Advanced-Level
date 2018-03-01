c9231c80715d243119e2644202e92e6603369ff7
L <462650965@qq.com>
2018-03-01 09:33:43 +0800

    -

--- 1112. Stucked Keyboard (20).cpp
+++ 1112. Stucked Keyboard (20).cpp
@@ -1,48 +1,35 @@
 /*
 题目地址：https://www.patest.cn/contests/pat-a-practise/1112
-思路：一定要先判断哪些键是坏的
+思路：只要满足连续相同字符个数不是k的整数倍，那么一定是好键
 */
-#include <iostream>
-#include <vector>
-#include <cstring> 
-#include <algorithm>
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
-bool isStucked[128],check[128],iscollect[128];
-int k;
-char str[1024];
-string possKey,ans;
-void stuckedKey(char str[],int n)
-{
-	int cur=0,next=0;
-	while(next!=n)
-	{
-		int idx=str[cur];
-		while(next!=n&&str[cur]==str[next]) ++next;
-		if((next-cur)%k) check[idx]=true;//只要有余数不为0的，那么肯定是好的
-		if(!check[idx]) isStucked[idx]=true;
-		else isStucked[idx]=false;
-		cur=next;
-	}
-	for(int i=0;i<n;++i)
-	{
-		ans+=str[i];
-		if(isStucked[str[i]-'\0'])
-		{
-			if(!iscollect[str[i]-'\0'])
-			{
-				possKey+=str[i];
-				iscollect[str[i]-'\0']=true;
-			}
-			i+=k-1;
-		}
-	}
-}
+bool isWell[178],isprt[178];
+int k,i;
+char keys[1024];
+string poskeys,ans;
 int main()
 {
-	scanf("%d\n",&k);
-	gets(str);
-	stuckedKey(str,strlen(str));
-	cout<<possKey<<endl<<ans;
+	scanf("%d%s",&k,keys);
+	while(keys[i])
+	{
+		int j=i+1;
+		while(keys[j]==keys[i]) ++j;
+		if((j-i)%k) isWell[keys[i]-'\0']=1;
+		i=j;
+	}
+	i=0;
+	while(keys[i])
+	{
+		int w=keys[i];
+		ans+=w;
+		if(isWell[w]) ++i;
+		else
+		{
+			if(!isprt[w]) poskeys+=w,isprt[w]=1;
+			i+=k;
+		}
+	}
+	cout<<poskeys<<endl<<ans<<endl;
 	return 0;
 }
