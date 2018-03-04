c8268cc2ec2b9a3f81630847cb1542551271021e
L <462650965@qq.com>
2018-03-04 16:02:39 +0800

    -

--- 1126. Eulerian Path (25).cpp
+++ 1126. Eulerian Path (25).cpp
@@ -1,39 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <cstdio> 
-#include <algorithm>
-#include <map> 
+#include <bits/stdc++.h>
 using namespace std;
-vector<vector<int>> v; 
-vector<bool> visit;
-int cnt;
-void dfs(int u)
-{
-	visit[u]=true;++cnt;
-	for(auto x:v[u]) if(!visit[x]) dfs(x);
-} 
+vector<int> v[512];
+int n,m,vertexNums,odd;
+bool visit[512];
+void dfs(int x){
+	visit[x]=1;
+	vertexNums++;
+	for(int y:v[x]){
+		if(!visit[y])dfs(y);
+	}
+}
 int main()
 {
-    int n,m;
-    cin>>n>>m;
-    v.resize(n+1);
-    visit.resize(n+1);
-    for(int i=0;i<m;++i)
-    {
-    	int u,w;
-    	cin>>u>>w;
-    	v[u].push_back(w);
-    	v[w].push_back(u);
-    }
-    dfs(1);
-    int odd=0;
-    for(int i=1;i<=n;++i)
-    {
-    	i==n?cout<<v[i].size()<<endl:cout<<v[i].size()<<" ";
-    	if(v[i].size()%2==1) odd++;
-    }
-    if(odd==0&&cnt==n) cout<<"Eulerian";
-    else if(odd==2&&cnt==n) cout<<"Semi-Eulerian";
-    else cout<<"Non-Eulerian";
-    return 0;
+	scanf("%d %d",&n,&m);
+	for(int i=0;i<m;++i){
+		int a,b;
+		scanf("%d %d",&a,&b);
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	dfs(1);
+	for(int i=1;i<=n;++i){
+		if(v[i].size()%2) ++odd;
+		i==n?printf("%lld\n",v[i].size()):printf("%lld ",v[i].size());
+	}
+	if(odd==0&&vertexNums==n) printf("Eulerian\n");
+	else if(odd==2&&vertexNums==n) printf("Semi-Eulerian\n");
+	else printf("Non-Eulerian\n");
+	return 0;
 }
