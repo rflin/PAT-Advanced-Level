193dd9c18732063fc63a34acba5a2eab4b50ba81
L <462650965@qq.com>
2018-03-07 10:11:21 +0800

    -

--- 1034. Head of a Gang (30).cpp
+++ 1034. Head of a Gang (30).cpp
@@ -1,77 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-struct node
-{
-	string name;
-	int size;
-	node(string str,int a):name(str),size(a){}
-	bool operator<(const node &x)
-	{
-		return name<x.name;
-	}
-};
-
-map<string,int> nametoidx;
-map<int,string> idxtoname;
-vector<vector<int>> v(10240);
-int cnttime[10240];
-bool vis[10240];
-int n,k,idx=0;
-int totaltime,cnt,head,maxtime;
-vector<node> ans;
-
-void dfs(int cur)
-{
-	if(vis[cur]) return;
-	vis[cur]=true;
-	cnt++;
-	totaltime+=cnttime[cur];
-	maxtime=maxtime>cnttime[cur]?maxtime:cnttime[head=cur];
-	for(auto x:v[cur])
-	{
-		if(!vis[x]) dfs(x);
+vector<string> idx2name;
+map<string,int> name2idx,result;
+vector<int> v[2048];
+int n,k,indexs=0,val[2048];
+int getidx(string &name){
+	if(name2idx.count(name)){
+		return name2idx[name];
+	}else{
+		name2idx[name]=indexs++;
+		idx2name.push_back(name);
+		return indexs-1;
 	}
 }
-int main(int argc, char const *argv[])
+void dfs(int u,vector<bool> &visit,int &maxvtx,int &maxval,int &nums,int &sums){
+	if(visit[u]) return;
+	visit[u]=1;
+	++nums;
+	sums+=val[u];
+	if(val[u]>maxval){
+		maxval=val[u];
+		maxvtx=u;
+	}
+	for(int x:v[u]){
+		if(!visit[x]) dfs(x,visit,maxvtx,maxval,nums,sums);
+	}
+}
+int main()
 {
 	cin>>n>>k;
 	for(int i=0;i<n;++i)
 	{
-		string str1,str2;
-		int idx1,idx2,w;
-		cin>>str1>>str2>>w;
-		if(nametoidx.count(str1)) idx1=nametoidx[str1];
-		else
-		{
-			idx1=nametoidx[str1]=idx;
-			idxtoname[idx++]=str1;
-		}
-		if(nametoidx.count(str2)) idx2=nametoidx[str2];
-		else
-		{
-			idx2=nametoidx[str2]=idx;
-			idxtoname[idx++]=str2;
-		}
-		v[idx1].push_back(idx2);
-		v[idx2].push_back(idx1);
-		cnttime[idx1]+=w;
-		cnttime[idx2]+=w;
+		string a,b;
+		int w,idxa,idxb;
+		cin>>a>>b>>w;
+		idxa=getidx(a);
+		idxb=getidx(b);
+		v[idxa].push_back(idxb);
+		v[idxb].push_back(idxa);
+		val[idxa]+=w,val[idxb]+=w;
 	}
-	for(int i=0;i<idx;++i)
-	{
-		totaltime=0;
-		cnt=0;
-		maxtime=-1;
-		head=-1;
-		dfs(i);
-		if(cnt>2&&totaltime>2*k) ans.push_back(node(idxtoname[head],cnt));
+	vector<bool> visit(indexs,0);
+	for(int i=0;i<indexs;++i){
+		int maxvtx=i,maxval=val[i],nums=0,sums=0;
+		dfs(i,visit,maxvtx,maxval,nums,sums);
+		if(nums>2&&sums>2*k){
+			result[idx2name[maxvtx]]=nums;
+		}
 	}
-	sort(ans.begin(),ans.end());
-	cout<<ans.size()<<endl;
-	for(auto x:ans)
-		cout<<x.name<<" "<<x.size<<endl;
+	cout<<result.size()<<endl;
+	for(auto &x:result){
+		cout<<x.first<<" "<<x.second<<endl;
+	}
 	return 0;
 }
