bd36d7993b1199d7cbc4f7d644ad9fd7b881691e
L <462650965@qq.com>
2018-03-07 21:18:55 +0800

    -

--- 1123. Is It a Complete AVL Tree (30).cpp
+++ 1123. Is It a Complete AVL Tree (30).cpp
@@ -1,27 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map> 
-#include <queue>
-#include <string> 
+#include <bits/stdc++.h>
 using namespace std;
 struct TreeNode
 {
 	int val;
 	TreeNode *left,*right;
-	TreeNode(int v=0,TreeNode *l=NULL,TreeNode *r=NULL):val(v),left(l),right(r){}
+	TreeNode(int v):val(v),left(NULL),right(NULL){}
 };
-int n,k;
 int height(TreeNode *r)
 {
 	return r==NULL?0:max(height(r->left),height(r->right))+1;
-}
-TreeNode* rRotate(TreeNode *r)
-{
-	TreeNode *p=r->left;
-	r->left=p->right;
-	p->right=r;
-	return p;
 }
 TreeNode* lRotate(TreeNode *r)
 {
@@ -30,82 +17,77 @@ TreeNode* lRotate(TreeNode *r)
 	p->left=r;
 	return p;
 }
+TreeNode* rRotate(TreeNode *r)
+{
+	TreeNode *p=r->left;
+	r->left=p->right;
+	p->right=r;
+	return  p;
+}
 TreeNode* lrRotate(TreeNode *r)
 {
 	r->left=lRotate(r->left);
 	return rRotate(r);
-} 
+}
 TreeNode* rlRotate(TreeNode *r)
 {
 	r->right=rRotate(r->right);
 	return lRotate(r);
-} 
-TreeNode* insert(TreeNode *r,int k)
+}
+TreeNode* insert(int val,TreeNode *r)
 {
-	if(r==NULL)
-	{
-		TreeNode *p=new TreeNode(k);
-		return r=p;
-	}
-	else if(r->val>k)
-	{
-		r->left=insert(r->left,k);
-		if(abs(height(r->left)-height(r->right))>1)
-		{
-			r=(k>r->left->val)?lrRotate(r):rRotate(r);
+	if(r==NULL) return new TreeNode(val);
+	if(r->val<val){
+		r->right=insert(val,r->right);
+		if(abs(height(r->left)-height(r->right))>1){
+			r=(r->right->val<val)?lRotate(r):rlRotate(r);
 		}
 	}
-	else 
-	{
-		r->right=insert(r->right,k);
-		if(abs(height(r->left)-height(r->right))>1)
-		{
-			r=(k>r->right->val)?lRotate(r):rlRotate(r);
+	else{
+		r->left=insert(val,r->left);
+		if(abs(height(r->left)-height(r->right))>1){
+			r=(r->left->val>val)?rRotate(r):lrRotate(r);
 		}
 	}
 	return r;
 }
-bool levtra(TreeNode *r)
+void levtra(TreeNode *r,int w,bool &iscbt)
 {
-	bool ret=true;
-	int w=0;
 	queue<TreeNode*> q;
 	q.push(r);
-	while(!q.empty())
-	{
+	while(!q.empty()){
 		TreeNode *s=q.front();
 		q.pop();
-		if(s)
-		{
-			++w==n?cout<<s->val:cout<<s->val<<" ";
+		if(s){
+			--w?printf("%d ",s->val):printf("%d\n",s->val);;
 			q.push(s->left);
 			q.push(s->right);
 		}
 		else break;
 	}
-	if(w<n) ret=false; 
-	while(!q.empty())
-	{
+	iscbt=w?0:1;
+	while(!q.empty()){
 		TreeNode *s=q.front();
 		q.pop();
-		if(s)
-		{
-			++w==n?cout<<s->val:cout<<s->val<<" ";
+		if(s){
+			--w?printf("%d ",s->val):printf("%d\n",s->val);;
 			q.push(s->left);
 			q.push(s->right);
 		}
 	}
-	return ret;
 }
 int main()
 {
-    TreeNode *r=NULL;
-    cin>>n;
-    for(int i=0;i<n;++i)
-    {
-    	cin>>k;
-    	r=insert(r,k);
+	TreeNode *r=NULL;
+	int n;
+	scanf("%d",&n);
+	for(int i=0;i<n;++i){
+		int val;
+		scanf("%d",&val);
+		r=insert(val,r);
 	}
-	levtra(r)?cout<<"\nYES":cout<<"\nNO";
-    return 0;
+	bool iscbt=0;
+	levtra(r,n,iscbt);
+	iscbt?printf("YES\n"):printf("NO\n");
+	return 0;
 }
