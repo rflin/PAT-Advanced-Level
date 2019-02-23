069910eea24ff8e37cc96fd92635625f168bcc4e
L <462650965@qq.com>
2019-02-23 22:43:56 +0800

    -

--- 1152 Google Recruitment (20).cpp
+++ 1152 Google Recruitment (20).cpp
@@ -1,24 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-bool isprime(int k){
-    if(k == 0) return false;
-    for(int i = 2; i * i <= k; ++i){
-        if(k % i == 0) return false;
+
+bool isprime(int n){
+    if(n == 0 || n == 1) return false;
+    for(int i = 2; i * i <= n; ++i){
+        if(n % i == 0) return false;
     }
     return true;
 }
 int main()
 {
+    int n, m;
     string nums;
-    int L, n;
-    cin >> L >> n >> nums;
-    for(int i = 0; i <= L - n; ++i){
-        string x = nums.substr(i, n);
-        if(isprime(stol(x))){
+    cin >> n >> m >> nums;
+    for(int i = 0; i <= n - m; ++i){
+        string x = nums.substr(i, m);
+        if(isprime(stoi(x))){
             cout << x << endl;
             return 0;
         }
     }
-    cout << "404" <<endl;
+    cout << "404" << endl;
     return 0;
 }
