6f4a5e6519598cb246e87646dda76def9aec0cf8
L <462650965@qq.com>
2019-02-26 20:13:01 +0800

    -

--- 1107. Social Clusters (30).cpp
+++ 1107. Social Clusters (30).cpp
@@ -1,41 +1,47 @@
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <functional>
+#include <bits/stdc++.h>
+
 using namespace std;
-int person[1000+1],hobby[1000+1],n;
+
+int persHob[1024];
+
+int finds(int x, vector<int> &parents){
+    return x == parents[x] ? x: parents[x] = finds(parents[x], parents);
+}
 int main()
 {
-	function<int(int,int[])> finds=[&finds](int x,int parents[]){return parents[x]==x?x:parents[x]=finds(parents[x],parents);};
-	cin>>n;
-	for(int i=1;i<=1000;++i)
-		hobby[i]=i;
-	for(int i=1;i<=n;++i)
-	{
-		int m,head,cur;
-		scanf("%d: %d",&m,&head);
-		person[i]=head;
-		for(int k=0;k<m-1;++k)
-		{
-			scanf("%d",&cur);
-			int hp=finds(head,hobby);
-			int cp=finds(cur,hobby);
-			if(hp!=cp) hobby[cp]=hp;
-		}
-	}
-	int ans[1000+1]={0};
-	for(int i=1;i<=n;++i)
-		ans[finds(person[i],hobby)]++;
-	vector<int> v;
-	for(int i=1;i<=1000;++i)
-		if(ans[i]) v.push_back(ans[i]);
-	sort(v.begin(),v.end(),greater<int>());
-	cout<<v.size()<<endl;
-	for(auto it=v.begin();it!=v.end();++it)
-	{
-		cout<<*it;
-		if(it!=v.end()-1)cout<<" ";
-	}
-	return 0;
+    vector<int> parents(1024);
+    for(int i = 1; i <= 1000; ++i){
+        parents[i] = i;
+    }
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i){
+        int k, fir;
+        scanf("%d: %d", &k, &fir);
+        persHob[i] = fir;
+        --k;
+        while(k--){
+            int x;
+            scanf("%d", &x);
+            int L1 = finds(fir, parents);
+            int L2 = finds(x, parents);
+            if(L1 != L2) parents[L2] = L1;
+        }
+    }
+    map<int, int> cnts;
+    for(int i = 1; i <= n; ++i){
+        int L = finds(persHob[i], parents);
+        cnts[L]++;
+    }
+    vector<int> res;
+    for(auto e: cnts){
+        res.push_back(e.second);
+    }
+    sort(res.begin(), res.end(), greater<int>());
+    int sz = res.size();
+    printf("%d\n", sz);
+    for(int i = 0; i < sz; ++i){
+        printf("%d%c", res[i], i == sz - 1 ? '\n' : ' ');
+    }
+    return 0;
 }
