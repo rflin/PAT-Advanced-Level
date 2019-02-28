753a3df6abc77ca2fff47be5324db94c184428fb
L <462650965@qq.com>
2019-02-28 20:35:46 +0800

    -

--- 1016. Phone Bills (25).cpp
+++ 1016. Phone Bills (25).cpp
@@ -1,76 +1,69 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-struct node
-{
-	char name[32];
-	int month,dd,hh,mm;
-	int time;
-	bool status;
-}s[1024];
-int toll[24];
-int daytoll=0;
-bool cmp(const node &a,const node &b)
-{
-	int ret=strcmp(a.name,b.name);
-	return ret==0?a.time<b.time:ret<0;
+struct node{
+    int m, d, hh, mm;
+    int t;
+    bool online;
+};
+
+int toll[24], n, daytoll;
+
+map<string, vector<node>> mp;
+
+bool cmp(const node &a, const node &b){
+    return a.t < b.t;
 }
-double cntBills(int sd,int sh,int sm,int ed,int eh,int em)
-{
-    double sbill=0.0,ebill=0.0;
-    sbill+=(sd-1)*60*daytoll;
-    ebill+=(ed-1)*60*daytoll;
-    for(int i=0;i<sh;++i)
-        sbill+=toll[i]*60;
-    for(int i=0;i<eh;++i)
-        ebill+=toll[i]*60;
-    sbill+=sm*toll[sh];
-    ebill+=em*toll[eh];
-    return (ebill-sbill);
+double mkbill(node &a){
+    int d = a.d, h = a.hh, m = a.mm;
+    double bill = (d - 1) * 60 * daytoll;
+    for(int i = 0; i < h; ++i){
+        bill += 60 * toll[i];
+    }
+    bill += m * toll[h];
+    return bill / 100;
 }
+
+void sol(const char name[], vector<node> &v){
+    bool flag = true, tag = false;
+    int sz = v.size(), i = 0;
+    double sum = 0.0;
+    while(i < sz){
+        if(i + 1 < sz && v[i].online && !v[i + 1].online){
+            tag = true;
+            if(flag){
+                printf("%s %02d\n", name, v[0].m);
+                flag = false;
+            }
+            int j = i + 1;
+            double billcnts = mkbill(v[j]) - mkbill(v[i]);
+            printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", v[i].d, v[i].hh, v[i].mm, v[j].d, v[j].hh, v[j].mm, v[j].t - v[i].t, billcnts);
+            sum += billcnts;
+            i += 2;
+        }
+        else ++i;
+    }
+    if(tag) printf("Total amount: $%.2f\n", sum);
+}
+
 int main()
 {
-	int n;
-	for(int i=0;i<24;++i)
-	{
-		scanf("%d",&toll[i]);
-		daytoll+=toll[i];
-	}
-	scanf("%d",&n);
-	for(int i=0;i<n;++i)
-	{
-		char str[32];
-		scanf("%s %d:%d:%d:%d %s",s[i].name,&s[i].month,&s[i].dd,&s[i].hh,&s[i].mm,str);
-		s[i].time=s[i].dd*24*60+s[i].hh*60+s[i].mm;
-		s[i].status=(strcmp(str,"on-line")==0)?1:0;
-	}
-	sort(s,s+n,cmp);
-	bool tag=true;
-	double totalbill=0.0;
-	char curname[32]="";
-	for(int i=0;i<n;++i)
-	{
-		int ret=strcmp(curname,s[i].name);
-		if(ret!=0)
-		{
-			if(totalbill)printf("Total amount: $%.2lf\n",totalbill);
-			strcpy(curname,s[i].name);
-			ret=0;
-			tag=true;
-			totalbill=0.0;
-		}
-		if(ret==0&&s[i].status&&strcmp(curname,s[i+1].name)==0&&!s[i+1].status)
-		{
-			if(tag==true)
-			{
-				printf("%s %02d\n",s[i].name,s[i].month);
-				tag=false;
-			}
-			double bills=cntBills(s[i].dd,s[i].hh,s[i].mm,s[i+1].dd,s[i+1].hh,s[i+1].mm)/100;
-			printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2lf\n",s[i].dd,s[i].hh,s[i].mm,s[i+1].dd,s[i+1].hh,s[i+1].mm,s[i+1].time-s[i].time,bills);
-			totalbill+=bills;
-			++i;
-		}
-	}
-	if(totalbill)printf("Total amount: $%.2lf\n",totalbill);
+    for(int i = 0; i < 24; ++i){
+        scanf("%d", toll + i);
+        daytoll += toll[i];
+    }
+    scanf("%d", &n);
+    for(int i = 0; i < n; ++i){
+        char name[24], status[24];
+        node x;
+        scanf("%s %d:%d:%d:%d %s", name, &x.m, &x.d, &x.hh, &x.mm, status);
+        x.t = (x.d - 1) * 24 * 60 + x.hh * 60 + x.mm;
+        x.online = status[1] == 'n';
+        mp[string(name)].push_back(x);
+    }
+    for(auto &x: mp){
+        sort(x.second.begin(), x.second.end(), cmp);
+        sol(x.first.c_str(), x.second);
+    }
     return 0;
 }
