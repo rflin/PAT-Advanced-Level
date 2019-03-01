91559e9d9bce5c3483ffb156af7b97044d75be5d
L <462650965@qq.com>
2019-03-01 16:14:16 +0800

    -

--- 1118. Birds in Forest (25).cpp
+++ 1118. Birds in Forest (25).cpp
@@ -1,41 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
 using namespace std;
-vector<int> sets(10240);
-int n,q,birdscnt,treecnt;
-int find(int x)
-{
-	return sets[x]==x?x:sets[x]=find(sets[x]);
+int birdsnum = -1;
+int parents[11024];
+int finds(int x){
+    return x == parents[x] ? x: parents[x] = finds(parents[x]);
 }
 int main()
 {
-	cin>>n;
-	for(int i=0;i<=10000;++i) sets[i]=i;
-	for(int i=0;i<n;++i)
-	{
-		int m,h,x;
-		cin>>m>>h;
-		birdscnt=max(birdscnt,h);
-		for(int i=0;i<m-1;++i)
-		{
-			cin>>x;
-			birdscnt=max(birdscnt,x);
-			int l1=find(h),l2=find(x);
-			if(l1!=l2) sets[l2]=l1;
-		}
-	}
-	for(int i=1;i<=birdscnt;++i)
-	{
-		if(sets[i]==i)++treecnt;
-	}
-	cout<<treecnt<<" "<<birdscnt<<endl;
-	cin>>q;
-	while(q--)
-	{
-		int b1,b2;
-		cin>>b1>>b2;
-		find(b1)==find(b2)?cout<<"Yes\n":cout<<"No\n";
-	}
-	return 0;
+    int n;
+    for(int i = 1; i <= 10000; ++i) parents[i] = i;
+    scanf("%d", &n);
+    vector<int> lead(n);
+    for(int i = 0; i < n; ++i){
+        int k;
+        scanf("%d %d", &k, &lead[i]);
+        birdsnum = max(birdsnum, lead[i]);
+        k--;
+        while(k--){
+            int x;
+            scanf("%d", &x);
+            birdsnum = max(birdsnum, x);
+            int L1 = finds(lead[i]), L2 = finds(x);
+            if(L1 != L2) parents[L2] = L1;
+        }
+    }
+    map<int, int> cnt;
+    for(int i = 0; i < n; ++i){
+        cnt[finds(lead[i])] = 1;
+    }
+    printf("%d %d\n", (int)cnt.size(), birdsnum);
+    int q;
+    scanf("%d", &q);
+    while(q--){
+        int x, y;
+        scanf("%d %d", &x, &y);
+        int L1 = finds(x), L2 = finds(y);
+        if(L1 != L2) printf("No\n");
+        else printf("Yes\n");
+    }
+    return 0;
 }
