31e0e4722c8ab8a6a3648bd77cadf1e8d2d08cf9
L <462650965@qq.com>
2019-03-14 18:09:00 +0800

    -

--- 1086. Tree Traversals Again (25).cpp
+++ 1086. Tree Traversals Again (25).cpp
@@ -1,42 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-#include <algorithm>
-using namespace std;
-int n,s;
-vector<int> ino,pre;
-stack<int> stk;
+#include <bits/stdc++.h>
 
-void build(int preL,int preR,int inoL,int inoR)
-{
-	if(preL>preR) return;
-	int e=pre[preL],idx=inoL,pdx=preL;
-	while(idx<=inoR&&e!=ino[idx]) idx++,pdx++;
-	build(preL+1,pdx,inoL,idx-1);
-	build(pdx+1,preR,idx+1,inoR);
-	++s!=n?cout<<e<<" ":cout<<e;
+using namespace std;
+
+stack<int> stk;
+int n;
+vector<int> pre, ino, pos;
+
+void build(int preL, int preR, int inoL, int inoR){
+    if(preL > preR) return;
+    int e = pre[preL], idx = inoL;
+    while(e != ino[idx]) ++idx;
+    build(preL + 1, preL + 1 + idx - 1 - inoL, inoL, idx - 1);
+    build(preR - inoR + idx + 1, preR, idx + 1, inoR);
+    pos.push_back(e);
 }
 int main()
 {
-	cin>>n;
-	for(int i=0;i<2*n;++i)
-	{
-		string str;
-		int x;
-		cin>>str;
-		if(str=="Push")
-		{
-			cin>>x;
-			pre.push_back(x);
-			stk.push(x);
-		}
-		else
-		{
-			ino.push_back(stk.top());
-			stk.pop();
-		}
-	}
-	build(0,n-1,0,n-1);
-	return 0;
+    cin >> n;
+    for(int i = 0; i < 2 * n; ++i){
+        string op;
+        cin >> op;
+        if(op == "Push"){
+            int val;
+            cin >> val;
+            stk.push(val);
+            pre.push_back(val);
+        }
+        else{
+            ino.push_back(stk.top());
+            stk.pop();
+        }
+    }
+    build(0, n - 1, 0, n - 1);
+    int sz = pos.size();
+    for(int i = 0; i < sz; ++i){
+        printf("%d%c", pos[i], i == n - 1 ? '\n': ' ');
+    }
+    return 0;
 }
